48da3a3d0eaffcf441f260689dca919b03a29992
qluo1 <samuel.luo1@gmail.com>
2013-07-14 17:50:06 +1000

    update readme, tested with latest batch_run.bat on my laptop

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 smaeoduploader
 ==============
 
-sma eod uploader
+sma end of day uploader
 
 
 Those scripts extract SMA 4000TL solar production data and upload them into www.pvoutput.org at end of the day in batch mode.
@@ -10,6 +10,6 @@ here is my daily output at pvoutput.org
 
 http://www.pvoutput.org/list.jsp?userid=1865
 
-Developed in Python. 
+Developed in Python2, currently not working for Python3
 
 original code located: http://code.google.com/p/smaeoduploader/
--- batch_run.bat
+++ batch_run.bat
@@ -1,12 +1,12 @@
 
 
-python sma_eod_loader.py -e -r -d 20130701
-REM python sma_eod_loader.py -e -r -d 20130702
-REM python sma_eod_loader.py -e -r -d 20130703
-REM python sma_eod_loader.py -e -r -d 20130704
-REM python sma_eod_loader.py -e -r -d 20130705
-REM python sma_eod_loader.py -e -r -d 20130706
-REM python sma_eod_loader.py -e -r -d 20130707
+python sma_eod_loader.py -e -r -d 20130708
+python sma_eod_loader.py -e -r -d 20130709
+python sma_eod_loader.py -e -r -d 20130710
+python sma_eod_loader.py -e -r -d 20130711
+python sma_eod_loader.py -e -r -d 20130712
+python sma_eod_loader.py -e -r -d 20130713
+python sma_eod_loader.py -e -r -d 20130714
 rem python sma_eod_loader.py -e -r -d 20130610
 
 
